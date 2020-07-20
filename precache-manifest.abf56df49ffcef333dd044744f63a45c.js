c523de7a7b56a8b8f280bc32d63acd5064f2251c
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-07-20 20:39:56 +0200

    Updates

---
+++ precache-manifest.abf56df49ffcef333dd044744f63a45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0a2635f9e6d4d30a820259f71e1510",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "b8e8dc13ac88a11e312f",
+    "url": "/todo-list-react/static/css/main.0ba6a01a.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e8dc13ac88a11e312f",
+    "url": "/todo-list-react/static/js/main.335ea52e.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
