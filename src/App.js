84c896c6bd4969ba18fed2d30be973a3a96ba925
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-07-21 21:12:59 +0200

    fix bugs - key and missing logo file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Container from "./Container";
 
 const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
-  { id: 1, content: "zjeść obiad", done: true },
+  { id: 2, content: "zjeść obiad", done: true },
 ]
 
 let isDoneHide = false;
--- src/TasksList/index.js
+++ src/TasksList/index.js
@@ -2,25 +2,26 @@ import React from "react";
 import "./style.css";
 
 const TasksList = ({ tasks, isDoneHide }) => (
-    <ul className="tasksList">
-        {tasks.map(task => (
-            <li className=
-                {`tasksList__item${isDoneHide && task.done ? " tasksList__item--hidden" : ""}`}
-            >
-                <button className="tasksList__button tasksList__button--toggleDone">
-                    {task.done ? "✔" : ""}
+  <ul className="tasksList">
+    {tasks.map(task => (
+      <li
+        key={task.id}
+        className={`tasksList__item${isDoneHide && task.done ? " tasksList__item--hidden" : ""}`}
+      >
+        <button className="tasksList__button tasksList__button--toggleDone">
+          {task.done ? "✔" : ""}
+        </button>
+        <span className=
+          {`tasksList__taskContent${task.done ? " tasksList__taskContent--done" : ""}`}
+        >
+          {task.content}
+        </span>
+        <button className="tasksList__button tasksList__button--remove">
+          🗑
                 </button>
-                <span className=
-                    {`tasksList__taskContent${task.done ? " tasksList__taskContent--done" : ""}`}
-                >
-                    {task.content}
-                </span>
-                <button className="tasksList__button tasksList__button--remove">
-                    🗑
-                </button>
-            </li>
-        ))}
-    </ul>
+      </li>
+    ))}
+  </ul>
 );
 
 export default TasksList;
