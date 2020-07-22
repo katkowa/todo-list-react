6f0052b26c29ef5675d9dcafaab2bc81317d2347
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-07-22 22:18:33 +0200

    add new task interaction

--- src/App.js
+++ src/App.js
@@ -38,6 +38,18 @@ function App() {
       })));
   }
 
+  const addNewTask = (content) => {
+    setTasks(tasks =>
+      [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        }
+      ]);
+  }
+
   return (
     <div className="App">
       <Container>
@@ -45,7 +57,7 @@ function App() {
 
         <Section
           title="Dodaj nowe zadanie"
-          body={<Form />}
+          body={<Form addNewTask={addNewTask} />}
         />
 
         <Section
