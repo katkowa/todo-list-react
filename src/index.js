3c10c4ea20aa8f3152a72901d896fdabeda3ae0d
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-08-27 22:20:58 +0200

    add GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "styled-components";
-import './index.css';
+import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { theme } from "./theme";
@@ -9,6 +9,7 @@ import { theme } from "./theme";
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,10 @@ export const theme = {
     color: {
         teal: "#008080",
         alto: "#ddd",
+        gallery: "#eee",
         white: "#fff",
         green: "green",
         crimson: "crimson",
-        
     },
     breakpoint: {
         mobileMax: 767,
